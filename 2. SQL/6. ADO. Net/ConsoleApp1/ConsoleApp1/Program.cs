0b05386b9a90a2a1632f4bba3d31dbfc1b2e5f3e
Andrei Tirsina <andrei.tirsina@amdaris.com>
2020-03-21 18:46:44 +0200

    ,

--- 2. SQL/6. ADO. Net/ConsoleApp1/ConsoleApp1/Program.cs
+++ 2. SQL/6. ADO. Net/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 
 namespace ConsoleApp1
 {
-   static class Program
+    static class Program
     {
         static void Main(string[] args)
         {
@@ -14,7 +15,7 @@ namespace ConsoleApp1
             {
                 sqlConnection.Open();
                 var sqlCommandText1 = "IF OBJECT_ID(N'Customers', N'U') IS NULL CREATE TABLE Customers(CustomerId int not null identity(1,1) primary key, First_Name NVARCHAR(40),Last_Name NVARCHAR(40))";
-                var sqlCommandText2 = "IF OBJECT_ID(N'Orders', N'U') IS NULL CREATE TABLE Orders(OrderId int not null identity(1,1) primary key, OrderName NVARCHAR(40), Price decimal(5,2) not null, Quantity int not null, CustomerId int foreign key references Customers(CustomerId))";
+                var sqlCommandText2 = "IF OBJECT_ID(N'Orders', N'U') IS NULL CREATE TABLE Orders(OrderId int not null identity(1,1) primary key, OrderName NVARCHAR(40), Price decimal not null, Quantity int not null, CustomerId int foreign key references Customers(CustomerId))";
                 using (var sqlCommand = new SqlCommand(sqlCommandText1, sqlConnection))
                 {
                     sqlCommand.ExecuteNonQuery();
@@ -27,53 +28,57 @@ namespace ConsoleApp1
 
             using (var sqlConnection = new SqlConnection(connectionString))
             {
-                sqlConnection.Open();
-                var customersDataAdapter = CustomerDataAdapter(new SqlConnection(connectionString));
-                var ordersDataAdapter = OrderDataAdapter(new SqlConnection(connectionString));
+                var customersDataAdapter = CustomerDataAdapter(sqlConnection);
+                var ordersDataAdapter = OrderDataAdapter(sqlConnection);
                 DataSet dataSet = new DataSet();
 
                 customersDataAdapter.Fill(dataSet, "Customers");
                 ordersDataAdapter.Fill(dataSet, "Orders");
-               // dataSet.Relations.Add("CustOrders", dataSet.Tables["Customers"].Columns["CustomerId"], dataSet.Tables["Orders"].Columns["CustomerId"]);
 
                 DataTable customersTable = dataSet.Tables["Customers"];
                 DataTable ordersTable = dataSet.Tables["Orders"];
 
-                customersTable.AddNewCustomer("John", "Doe");
-                customersTable.AddNewCustomer("Joshua", "Brooks");
-                customersTable.AddNewCustomer("Jane", "Arnold");
-                customersTable.AddNewCustomer("Patricia", "Burns");
-                customersTable.AddNewCustomer("Sabrina", "McBride");
-                customersTable.AddNewCustomer("Gordon", "Moody");
-                customersTable.AddNewCustomer("Tyrone", "Moody");
+                //customersTable.AddNewCustomer("John", "Doe");
+                //customersTable.AddNewCustomer("Joshua", "Brooks");
+                //customersTable.AddNewCustomer("Jane", "Arnold");
+                //customersTable.AddNewCustomer("Patricia", "Burns");
+                //customersTable.AddNewCustomer("Sabrina", "McBride");
+                //customersTable.AddNewCustomer("Gordon", "Moody");
+                //customersTable.AddNewCustomer("Tyrone", "Moody");
 
-                ordersTable.AddNewOrder("Apples", 25, 2, 1);
-                ordersTable.AddNewOrder("Smartphone", 2000, 1, 1);
-                ordersTable.AddNewOrder("Marshmallows", 30, 10, 1);
-                ordersTable.AddNewOrder("Cake", 100, 6, 3);
-                ordersTable.AddNewOrder("Pie", 25, 2, 7);
-                ordersTable.AddNewOrder("Apples", 25, 13, 5);
-                ordersTable.AddNewOrder("Smartphone", 3000, 2, 4); 
-                ordersTable.AddNewOrder("Cheese", 34, 8, 3);
-                ordersTable.AddNewOrder("Cola", 20, 5, 2);
-                ordersTable.AddNewOrder("Dress", 300, 1, 3);
+                //ordersTable.AddNewOrder("Apples", 25, 2, 1);
+                //ordersTable.AddNewOrder("Smartphone", 2000, 1, 1);
+                //ordersTable.AddNewOrder("Marshmallows", 30, 10, 1);
+                //ordersTable.AddNewOrder("Cake", 100, 6, 3);
+                //ordersTable.AddNewOrder("Pie", 25, 2, 7);
+                //ordersTable.AddNewOrder("Apples", 25, 13, 5);
+                //ordersTable.AddNewOrder("Smartphone", 3000, 2, 4);
+                //ordersTable.AddNewOrder("Cheese", 34, 8, 3);
+                //ordersTable.AddNewOrder("Cola", 20, 5, 2);
+                //ordersTable.AddNewOrder("Dress", 300, 1, 3);
+
+                //var deleteOrders = ordersTable.AsEnumerable().Where(x => x.Field<int>("CustomerId") == 1);
+                //foreach (var i in deleteOrders) i.Delete();
+
+                var updatingCustomer = customersTable.AsEnumerable().Where(x => x.Field<int>("CustomerId") == 1).FirstOrDefault();
+                updatingCustomer.SetField("First_Name", "Andrew");
+                updatingCustomer.SetField("Last_Name", "Johnson");
 
                 customersDataAdapter.Update(customersTable);
                 ordersDataAdapter.Update(ordersTable);
             }
-
         }
         public static SqlDataAdapter CustomerDataAdapter(SqlConnection sqlConnection)
         {
             SqlDataAdapter customersDataAdapter = new SqlDataAdapter();
 
-             //SelectCommand
+            //SelectCommand
             SqlCommand command = new SqlCommand("SELECT * FROM Customers", sqlConnection);
 
             customersDataAdapter.SelectCommand = command;
 
             //InsertComand
-            var insertText = "INSERT INTO Customer(First_Name, Last_Name) VALUES(@First_Name, @Last_Name)";
+            var insertText = "INSERT INTO Customers(First_Name, Last_Name) VALUES(@First_Name, @Last_Name)";
 
             SqlCommand insertCommand = new SqlCommand(insertText, sqlConnection);
             insertCommand.Parameters.Add("@First_Name", SqlDbType.NVarChar, 40, "First_Name");
@@ -82,12 +87,12 @@ namespace ConsoleApp1
             customersDataAdapter.InsertCommand = insertCommand;
 
             //UpdateComand
-            command = new SqlCommand( "UPDATE Customers SET First_Name = @First_Name, Last_Name = @Last_Name " +
+            command = new SqlCommand("UPDATE Customers SET First_Name = @First_Name, Last_Name = @Last_Name " +
                                       "WHERE CustomerId = @CustomerId", sqlConnection);
 
             command.Parameters.Add("@First_Name", SqlDbType.NVarChar, 40, "First_Name");
             command.Parameters.Add("@Last_Name", SqlDbType.NVarChar, 40, "Last_Name");
-            SqlParameter parameter = command.Parameters.Add("@CustomerId", SqlDbType.BigInt);
+            SqlParameter parameter = command.Parameters.Add("@CustomerId", SqlDbType.BigInt,sizeof(long),"CustomerId");
             parameter.SourceVersion = DataRowVersion.Original;
 
             customersDataAdapter.UpdateCommand = command;
@@ -115,9 +120,9 @@ namespace ConsoleApp1
 
             var insertCommand = new SqlCommand(insertText, sqlConnection);
             insertCommand.Parameters.Add("@OrderName", SqlDbType.NVarChar, 255, "OrderName");
-            insertCommand.Parameters.Add("@Price", SqlDbType.Decimal);
-            insertCommand.Parameters.Add("@Quantity", SqlDbType.Int);
-            insertCommand.Parameters.Add("@CustomerId", SqlDbType.Int);
+            insertCommand.Parameters.Add("@Price", SqlDbType.Decimal, sizeof(decimal), "Price");
+            insertCommand.Parameters.Add("@Quantity", SqlDbType.Int, sizeof(int), "Quantity");
+            insertCommand.Parameters.Add("@CustomerId", SqlDbType.Int, sizeof(int), "CustomerId");
 
             ordersDataAdapter.InsertCommand = insertCommand;
 
@@ -126,9 +131,9 @@ namespace ConsoleApp1
                                           "WHERE OrderID = @OrderID", sqlConnection);
 
             command.Parameters.Add("@OrderName", SqlDbType.NVarChar, 255, "OrderName");
-            command.Parameters.Add("@Price", SqlDbType.Decimal);
-            command.Parameters.Add("@Quantity", SqlDbType.Int);
-            command.Parameters.Add("@CustomerId", SqlDbType.Int);
+            command.Parameters.Add("@Price", SqlDbType.Decimal, sizeof(decimal), "Price");
+            command.Parameters.Add("@Quantity", SqlDbType.Int, sizeof(int), "Quantity");
+            command.Parameters.Add("@CustomerId", SqlDbType.Int, sizeof(int), "CustomerId");
             var parameter = command.Parameters.Add("@OrderId", SqlDbType.BigInt);
             parameter.SourceVersion = DataRowVersion.Original;
 
@@ -137,14 +142,13 @@ namespace ConsoleApp1
             //DeleteCommand
             command = new SqlCommand("DELETE FROM Orders WHERE OrderId = @OrderId", sqlConnection);
 
-            parameter = command.Parameters.Add("@OrderId", SqlDbType.BigInt);
+            parameter = command.Parameters.Add("@OrderId", SqlDbType.Int,sizeof(int),"OrderId");
             parameter.SourceVersion = DataRowVersion.Original;
 
             ordersDataAdapter.DeleteCommand = command;
 
             return ordersDataAdapter;
         }
-
         public static void AddNewCustomer(this DataTable customersTable, string first_Name, string last_Name)
         {
             DataRow customersRow = customersTable.NewRow();
@@ -152,9 +156,9 @@ namespace ConsoleApp1
             customersRow["First_Name"] = first_Name;
             customersRow["Last_Name"] = last_Name;
 
-            customersTable.ImportRow(customersRow);
+            customersTable.Rows.Add(customersRow);
         }
-        public static void AddNewOrder( this DataTable ordersTable,string orderName, decimal price, int quantity, int customerId)
+        public static void AddNewOrder(this DataTable ordersTable, string orderName, decimal price, int quantity, int customerId)
         {
             DataRow orderRow = ordersTable.NewRow();
 
@@ -163,7 +167,7 @@ namespace ConsoleApp1
             orderRow["Quantity"] = quantity;
             orderRow["CustomerId"] = customerId;
 
-            ordersTable.ImportRow(orderRow);
+            ordersTable.Rows.Add(orderRow);
         }
     }
 }
